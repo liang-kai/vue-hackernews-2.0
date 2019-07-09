428c95e874d83ed80e535786016946e9271ce281
liang-kai <lkaientan@gmail.com>
2019-07-09 13:05:50 +0800

    add travis

--- auto.sh
+++ auto.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
 echo 'begin sh'
+ssh -i ./deploy_key root@47.93.63.88 > /dev/null 2>&1 << eeooff
 cd /root/vue-hackernews-2.0/
 git pull origin master
 mkdir self
+exit
+eeooff
 echo 'travis build done!'
